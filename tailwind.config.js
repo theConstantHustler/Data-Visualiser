5adca350a8745a33d7f0d83b7041666ec3e29101
Nishanth Gunupudi <nishanth.gunupudi@auctree.com>
2024-01-13 22:36:31 +0530

    initial commit - project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Oswald", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
